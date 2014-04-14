ad4874aa3e0eaa58ea424b4841d695cf677ed604
Rich Bowman <BostonREB99@gmail.com>
2014-04-14 14:38:06 -0400

    Adds profiles controller and new page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ UpMinds::Application.routes.draw do
 
   root 'homes#show'
 
-  resources :users, only: [:show, :new, :edit]
+  resources :users, only: [:show] do
+    resources :profiles, only: [:show, :new, :edit]
+  end
 
   resource :dashboard, only: [:show]
+
+
 
 end
