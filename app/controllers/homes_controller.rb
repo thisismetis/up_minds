aa697ef669bcefff5375523bba58f40f2594283d
Rich Bowman <BostonREB99@gmail.com>
2014-04-14 14:09:33 -0400

    Adds has_profile to Users table

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,12 @@
 class HomesController < ApplicationController
+  before_action :goto_dashboard, if: :signed_in?
 
   def show
+  end
+
+  private
+  def goto_dashboard
+    redirect_to dashboard_path
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140414173947) do
+ActiveRecord::Schema.define(version: 20140414180646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: true do |t|
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.string   "email",                          null: false
-    t.string   "encrypted_password", limit: 128, null: false
+    t.datetime "created_at",                                     null: false
+    t.datetime "updated_at",                                     null: false
+    t.string   "email",                                          null: false
+    t.string   "encrypted_password", limit: 128,                 null: false
     t.string   "confirmation_token", limit: 128
-    t.string   "remember_token",     limit: 128, null: false
+    t.string   "remember_token",     limit: 128,                 null: false
     t.string   "first_name"
     t.string   "last_name"
     t.string   "telephone"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20140414173947) do
     t.string   "dribbble"
     t.string   "blog"
     t.string   "personal_site"
+    t.boolean  "has_profile",                    default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
