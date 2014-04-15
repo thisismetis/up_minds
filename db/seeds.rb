83b48440730d45ee9bc3086d65ba1b67eada6bd8
Dorothy Thurston <dorothy.thur@gmail.com>
2014-04-15 10:34:13 -0400

    Fix db seeding command

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 ["Python", "PHP", "JavaScript", "C++", "Ruby on Rails", "Ruby", "SQL", "Java", "C", "HTML", "CSS", "PostgreSQL", "mondoDB", "XML", "ASP", "RSS"].each do |skill|
-  Skill.find_or_create_by_name(skill)
+  Skill.find_or_create_by(name:skill)
 end
 
