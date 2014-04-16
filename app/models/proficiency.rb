dfefab22977ba4d4eb4bf2b4edfa1680edf57c4c
Rich Bowman <BostonREB99@gmail.com>
2014-04-16 15:32:13 -0400

    Add graphing of proficiencies

--- app/models/proficiency.rb
+++ app/models/proficiency.rb
@@ -3,6 +3,11 @@ class Proficiency < ActiveRecord::Base
   belongs_to :user
   belongs_to :skill
 
+  delegate :name, to: :skill, prefix: true
+
+  validates :user_id,
+    uniqueness: { scope: :skill_id }
+
   LEVELS = {
     "Familiar With" => "1",
     "Level 2" => "2",
