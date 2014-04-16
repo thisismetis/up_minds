4aa2833f1fcfcc1876edffce5f9556954067f076
Rich Bowman <BostonREB99@gmail.com>
2014-04-16 13:32:45 -0400

    Add user proficiencies

--- app/models/proficiency.rb
+++ app/models/proficiency.rb
@@ -1,4 +1,14 @@
 class Proficiency < ActiveRecord::Base
+
   belongs_to :user
   belongs_to :skill
+
+  LEVELS = {
+    "Familiar With" => "1",
+    "Level 2" => "2",
+    "Level 3" => "3",
+    "Level 4" => "4",
+    "Wizard" => "5"
+  }
+
 end
