32352266c166a7da58f0e6ff5de50dd4ed2b87c2
Lisa-Maria Mehta <jplisamaria@gmail.com>
2014-04-16 14:23:07 -0400

    Mark featured projects from project#index

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,15 @@
 class Project < ActiveRecord::Base
   belongs_to :user
+
+  def featured?
+    featured
+  end
+
+  def feature
+    self.featured = true
+  end
+
+  def unfeature
+    self.featured = false
+  end
 end
