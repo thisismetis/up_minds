07fb308a2ae7db640a7f554b1343fff633c6ae2d
Rich Bowman <BostonREB99@gmail.com>
2014-04-16 14:24:03 -0400

    move proficiency values into constant within proficiency model

--- app/models/proficiency.rb
+++ app/models/proficiency.rb
@@ -3,4 +3,12 @@ class Proficiency < ActiveRecord::Base
   belongs_to :user
   belongs_to :skill
 
+  LEVELS = {
+    "Familiar With" => "1",
+    "Level 2" => "2",
+    "Level 3" => "3",
+    "Level 4" => "4",
+    "Wizard" => "5"
+  }
+
 end
