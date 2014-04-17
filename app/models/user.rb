c9345f2bc656dcf4159723330fefe011263fd756
Lisa-Maria Mehta <jplisamaria@gmail.com>
2014-04-17 16:37:17 -0400

    Refactor to add user.name for displaying name OR email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ActiveRecord::Base
   has_many :skills, through: :proficiencies
   has_many :projects
 
+  def name
+    if first_name.present? && last_name.present?
+      "#{first_name} #{last_name}"
+    else
+      email
+    end
+  end
+
 end
