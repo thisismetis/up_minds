756807facb382e2bfa7d936e1955820a2fbd332f
Dorothy Thurston <dorothy.thur@gmail.com>
2014-04-17 17:12:49 -0400

    Add search on skills through proficiency

--- app/models/user_searcher.rb
+++ app/models/user_searcher.rb
@@ -4,7 +4,7 @@ class UserSearcher
   end
 
   def results
-    matching_users.order('created_at DESC')
+    matching_users
   end
 
   private
@@ -13,16 +13,27 @@ class UserSearcher
 
   def matching_users
     if @query
-      User.where(
-        "first_name ilike :query \
-         OR last_name ilike :query \
-         OR city ilike :query \
-         OR state ilike :query",
-        query: fuzzy_query
-      )
+       matched_by_profile + matched_by_skills
     else
       User.none
     end
+  end
+
+  def matched_by_profile
+    User.where(
+    "first_name ilike :query \
+     OR last_name ilike :query \
+     OR city ilike :query \
+     OR state ilike :query",
+    query: fuzzy_query
+    )
+  end
+
+  def matched_by_skills
+     User.joins(:proficiencies)
+    .joins(:skills)
+    .where("name ilike ?", fuzzy_query)
+    .uniq
   end
 
   def fuzzy_query
