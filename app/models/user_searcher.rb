124edaf43fe6412270e9f45de12dfe4fce757569
Dorothy Thurston <dorothy.thur@gmail.com>
2014-04-17 17:12:49 -0400

    Search returns sortable table

--- app/models/user_searcher.rb
+++ app/models/user_searcher.rb
@@ -4,25 +4,45 @@ class UserSearcher
   end
 
   def results
-    matching_users.order('created_at DESC')
+    find_users
   end
 
   private
 
   attr_reader :query
 
-  def matching_users
+  def find_users
     if @query
-      User.where(
-        "first_name ilike :query \
-         OR last_name ilike :query \
-         OR city ilike :query \
-         OR state ilike :query",
-        query: fuzzy_query
-      )
+      find_matching_users
     else
       User.none
     end
+  end
+
+  def find_matching_users
+    if skill_level_query?
+      find_by_level
+    else
+      find_by_text
+    end
+  end
+
+  def find_by_text
+    User.joins(:proficiencies).joins(:skills).where(
+      "first_name ilike :query \
+      OR last_name ilike :query \
+      OR city ilike :query \
+      OR state ilike :query \
+      OR name ilike :query", query: fuzzy_query
+    )
+  end
+
+  def skill_level_query?
+    @query =~ /\A\d+\z/
+  end
+
+  def find_by_level
+    User.joins(:proficiencies).where('level >= ?', query)
   end
 
   def fuzzy_query
