cd4efd81fa2ba51649165e99b12bbc2c56c083f2
Rich Bowman <BostonREB99@gmail.com>
2014-04-21 11:15:39 -0400

    Add Homepage

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,10 +2,11 @@ class HomesController < ApplicationController
   before_action :goto_dashboard, if: :signed_in?
 
   def show
+    @user = User.order("RANDOM()").limit(3)
   end
 
   private
-  
+
   def goto_dashboard
     redirect_to dashboard_path
   end
