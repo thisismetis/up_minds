5730d0361a687cd8e91c6bc8d21a929f5a288468
Dorothy Thurston <dorothy.thur@gmail.com>
2014-04-22 14:53:13 -0400

    Add delegate to method

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,4 +1,6 @@
 class Developer < User
   has_one :developer_profile
-  # accepts_nested_attributes_for :employer_profile
+  accepts_nested_attributes_for :employer_profile
+  delegate :github, :twitter, :linked_in, :bio,:personal_site, :blog, to: :developer_profile
+
 end
