f398828d288c5ef80c406716c7ead755e73ddafd
Lisa-Maria Mehta <jplisamaria@gmail.com>
2014-04-23 17:46:07 -0400

    Show watching on dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,7 @@
 class DashboardsController < ApplicationController
   def show
     @user = current_user
+    @watched_users = current_user.watched_users
+    @watchers = current_user.watchers
   end
 end
