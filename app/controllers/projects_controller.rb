c6a08a7021fb52f49abd3c2e961e583dc42607d6
Rich Bowman <BostonREB99@gmail.com>
2014-04-23 16:40:26 -0400

    remove unnecessary instance variables

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,27 +1,22 @@
 class ProjectsController < ApplicationController
   def index
-    @user = current_user
-    @projects = @user.projects
+    @projects = current_user.projects
   end
 
   def new
-    @user = current_user
     @project = Project.new
   end
 
   def create
-    @user = current_user
     @project = current_user.projects.create(project_params)
     redirect_to projects_path
   end
 
   def edit
-    @user = current_user
     @project = find_project
   end
 
   def update
-    @user = current_user
     @project = find_project
     @project.update(project_params)
     redirect_to projects_path
