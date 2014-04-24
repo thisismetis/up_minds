b6aa6220c62f43929e9951a47c017a3abc3eee97
Lisa-Maria Mehta <jplisamaria@gmail.com>
2014-04-24 14:45:50 -0400

    Show watching on dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,7 @@
 class DashboardsController < ApplicationController
   def show
     @user = current_user
+    @watched_users = WatchingRelationship.where(watcher_id: current_user.id)
+    @watchers = WatchingRelationship.where(watched_user_id: current_user.id)
   end
 end
