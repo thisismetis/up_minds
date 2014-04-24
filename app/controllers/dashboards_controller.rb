a089a49f3cf40f7a3760628011eb1d6574c8b5bd
Lisa-Maria Mehta <jplisamaria@gmail.com>
2014-04-24 15:44:24 -0400

    fix ugly referencing

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,9 @@
 class DashboardsController < ApplicationController
   def show
     @user = current_user
-    @watched_users = WatchingRelationship.where(watcher_id: current_user.id)
-    @watchers = WatchingRelationship.where(watched_user_id: current_user.id)
+    @watched_users = current_user.watched_users
+    @watched_users_old = WatchingRelationship.where(watcher_id: current_user.id)
+    @watchers = current_user.watchers
+    @watchers_old = WatchingRelationship.where(watched_user_id: current_user.id)
   end
 end
