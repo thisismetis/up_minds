b605506687202f79df3a144c93d6783d37178e6b
Rich Bowman <BostonREB99@gmail.com>
2014-04-24 15:36:20 -0400

    show random users on employer dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,9 @@
 class DashboardsController < ApplicationController
+
   def show
     @user = current_user
     @watched_users = current_user.watched_users
     @watchers = current_user.watchers
+    @random_user = Mind.order("RANDOM()").limit(3)
   end
 end
