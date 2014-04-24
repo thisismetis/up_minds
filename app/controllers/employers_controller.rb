27b2144140672ae80778f2cb6bc36ca6a430ebd3
Dorothy Thurston <dorothy.thur@gmail.com>
2014-04-24 15:51:29 -0400

    Add form fors for Profiles

--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -1,7 +1,37 @@
 class EmployersController < ApplicationController
   def show
+    @employer = Employer.find(params[:id])
+    @employer_profile = EmployerProfile.new
+  end
+
+  def create
+    @employer_profile = EmployerProfile.create(employer_profile_params)
+    redirect_to employer_path
   end
 
   def edit
+    @employer_profile = current_user.employer_profile
   end
+
+  def update
+    @employer_profile = find_employer_profile
+    @employer_profile.update(employer_profile_params)
+    redirect_to employer_path
+  end
+
 end
+
+private
+
+def employer_profile_params
+  params.require(:employer_profile).permit(
+    :company_name,
+    :company_url,
+    :user_id
+  )
+end
+
+def find_employer_profile
+  current_user.employer_profiles.find(params[:id])
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140423191551) do
+ActiveRecord::Schema.define(version: 20140424194644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20140423191551) do
   create_table "employer_profiles", force: true do |t|
     t.string   "company_name"
     t.string   "company_url"
-    t.integer  "user_id"
+    t.integer  "employer_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20140423191551) do
     t.string   "personal_site"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
+    t.integer  "mind_id"
   end
 
   create_table "proficiencies", force: true do |t|
